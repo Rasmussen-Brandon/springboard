4e876f97699bd8a9c77e689720cd66cc7ed182df
Luke Zachmann <luke@Lukes-MacBook-Pro-2.local>
2017-07-13 14:01:24 -0700

    fixed path mapping in docker run (deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 export CONTAINER_IMAGE_NAME=gcr.io/earthengine-project/datalab-ee:latest
-export WORKSPACE=${HOME}/Sandbox/datalab-ee
 
 docker run -it \
     -p "127.0.0.1:8081:8080" \
-    -v "$WORKSPACE:/content" \
+    -v "$(pwd)":"/content" \
     $CONTAINER_IMAGE_NAME
